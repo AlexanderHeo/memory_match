12003d2ec7ad87664238aa1310369d590f1e8cd0
JiKwan Alexander Heo <jikster712@gmail.com>
2019-11-15 15:40:55 -0800

    match-noflip/unmatch-flip, no click when two cards picked, stats box

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ $(document).ready(initializeApp);
 
 function initializeApp() {
 $(".cards").on("click", handleCardClick);
-// randomizeCardClass();
 }
 
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
 var max_matches = 9;
+var attempts = 0;
+var games_played = 0;
 
 function handleCardClick(event) {
     if ($(event.currentTarget).find(".cardBack").hasClass("hidden")) {
       return;
     }
-  // debugger;
-  // console.log("event", event);
   var clickedCardBack = $(event.currentTarget).find(".cardBack");
   var clickedCardFront = $(event.currentTarget).find(".cardFront");
   clickedCardBack.addClass("hidden");
-  // console.log('clickedCardBack:', clickedCardBack);
 
   if (!firstCardClicked) {
     firstCardClicked = clickedCardFront;
   } else {
     secondCardClicked = clickedCardFront;
+
+    if (firstCardClicked && secondCardClicked) {
+      $(".cards").off("click", handleCardClick);
+    }
+
     var firstCardImage = firstCardClicked.css("background-image");
     var secondCardImage = secondCardClicked.css("background-image");
 
     if (firstCardImage === secondCardImage) {
       matches++;
+      console.log("matches", matches);
       firstCardClicked = null;
       secondCardClicked = null;
+      $(".cards").on("click", handleCardClick);
       if (max_matches === matches) {
         $(".winModal").removeClass("hidden");
+        games_played++;
+        console.log("games_played:", games_played);
       }
     } else {
       setTimeout(function () {
@@ -41,17 +48,22 @@ function handleCardClick(event) {
       secondCardClicked.siblings().removeClass("hidden");
       firstCardClicked = null;
       secondCardClicked = null;
+      $(".cards").on("click", handleCardClick);
       }, 1500);
     }
+    attempts++;
+    console.log("attempts", attempts);
+    displayStats();
   }
 }
+function displayStats () {
+  var accur = calculateAccuracy(attempts, matches);
+  $(".statsPlayed").text(games_played);
+  $(".statsAttempt").text(attempts);
+  $(".statsAccuracy").text(accur + "%");
+}
 
-
-// randomize cardClasses and use jQuery to add them to proper divs
-// will need to use each class twice, and no more...
-// function randomizeCardClass () {
-//   var cardFrontClasses = [".css", ".docker", "gitHub", "html", "js", "mysql", "node", "php", "react"];
-
-//   var classIndex = Math.floor(Math.random() *10)+1;
-
-// }
+function calculateAccuracy (attempts, matches) {
+  var accuracy = Math.round((matches/attempts)*100);
+  return accuracy;
+}
